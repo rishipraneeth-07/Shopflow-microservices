629f6aa5fdd4c405e96654c81fbdc250bb2f193a
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-14 19:55:36 +0530

    feat:Add Create User Request dto record

---
+++ user-service/src/main/java/com/shopflow/userservice/dto/CreateUserRequest.java
@@ -0,0 +1,13 @@
+package com.shopflow.userservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserRequest (
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email")
+        String email
+){}
