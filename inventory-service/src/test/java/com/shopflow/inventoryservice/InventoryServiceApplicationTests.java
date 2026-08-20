5a8fcd8eb20544891f7a295ba5187928cefb4f47
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-20 17:32:03 +0530

    feat:Initialize inventory-service and configure sql properties

---
+++ inventory-service/src/test/java/com/shopflow/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopflow.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
