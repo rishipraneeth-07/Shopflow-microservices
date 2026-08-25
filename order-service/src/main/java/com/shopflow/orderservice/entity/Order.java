e87d0528b4a44e9e842416d876bfd4a3035b2459
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-25 22:42:10 +0530

    feat:add Order entity

---
+++ order-service/src/main/java/com/shopflow/orderservice/entity/Order.java
@@ -0,0 +1,30 @@
+package com.shopflow.orderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="orders")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+}
