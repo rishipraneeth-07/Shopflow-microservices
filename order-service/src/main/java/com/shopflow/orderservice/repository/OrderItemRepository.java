98bc53f14a91ab90e70a1668709012b8cbed7bdf
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-27 11:00:10 +0530

    feat:add OrderItem repository

---
+++ order-service/src/main/java/com/shopflow/orderservice/repository/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.shopflow.orderservice.repository;
+
+import com.shopflow.orderservice.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrderId(Long orderId);
+}
