6a5e7bb121d7302ad7f51935ae1d7f44bcf84cc5
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-30 11:09:05 +0530

    feat: add product retrieval through order service

--- order-service/src/main/java/com/shopflow/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/shopflow/orderservice/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.shopflow.orderservice.service;
 
+import com.shopflow.orderservice.dto.ProductResponse;
 import com.shopflow.orderservice.dto.UserResponse;
 
 public interface OrderService {
     UserResponse getUserById(Long id);
+    ProductResponse getProductById(Long id);
 }
--- order-service/src/main/java/com/shopflow/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/shopflow/orderservice/service/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.shopflow.orderservice.service;
 
+import com.shopflow.orderservice.client.ProductClient;
 import com.shopflow.orderservice.client.UserClient;
+import com.shopflow.orderservice.dto.ProductResponse;
 import com.shopflow.orderservice.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderServiceImpl  implements OrderService {
     private final UserClient userClient;
+    private final ProductClient productClient;
 
     @Override
     public UserResponse getUserById(Long id) {
         return userClient.getUserById(id);
+    }
+
+    @Override
+    public ProductResponse getProductById(Long id) {
+        return productClient.getProductById(id);
     }
 }
