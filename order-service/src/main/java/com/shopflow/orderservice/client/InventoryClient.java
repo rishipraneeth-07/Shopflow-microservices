dca5b1537b903623d3c5b707f69febea5bc25e19
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-08-31 23:26:48 +0530

    feat: add inventory service feign client

---
+++ order-service/src/main/java/com/shopflow/orderservice/client/InventoryClient.java
@@ -0,0 +1,14 @@
+package com.shopflow.orderservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "inventory-service",url = "http://localhost:8083")
+public interface InventoryClient {
+
+    @GetMapping("/inventory/{productId}/available")
+    Boolean isProductAvailable(@PathVariable Long productId,@RequestParam Integer quantity);
+
+}
