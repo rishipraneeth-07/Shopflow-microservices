7d92a7383228290484281d7a15c220ed6985d803
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-09-05 01:52:07 +0530

    feat: enable eureka service discovery for feign clients

--- order-service/src/main/java/com/shopflow/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/com/shopflow/orderservice/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory-service",url = "http://localhost:8083")
+@FeignClient(name = "inventory-service")
 public interface InventoryClient {
 
     @GetMapping("/inventory/{productId}/available")
