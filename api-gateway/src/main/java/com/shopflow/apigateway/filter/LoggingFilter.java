732a4c98021f970fc5d2d58234fcec77dccd1e22
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-09-10 21:39:50 +0530

    feat: add reactive gateway logging filter

--- api-gateway/src/main/java/com/shopflow/apigateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/com/shopflow/apigateway/filter/LoggingFilter.java
@@ -1,4 +1,24 @@
 package com.shopflow.apigateway.filter;
 
-public class LoggingFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("Incoming request"+""+exchange.getRequest().getMethod()+
+                ""+exchange.getRequest().getURI());
+        return chain.filter(exchange);
+    }
 }
