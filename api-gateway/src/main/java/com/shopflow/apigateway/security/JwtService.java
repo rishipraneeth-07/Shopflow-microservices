cf8fd4be7a755554d7be8f09da6a21fe07286be1
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-09-12 10:25:02 +0530

    feat: add JWT service for token generation and validation

--- api-gateway/src/main/java/com/shopflow/apigateway/security/JwtService.java
+++ api-gateway/src/main/java/com/shopflow/apigateway/security/JwtService.java
@@ -1,4 +1,44 @@
 package com.shopflow.apigateway.security;
 
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(Long userId){
+        return Jwts.builder()
+                .subject(userId.toString())
+                .issuedAt(new Date())
+                .expiration(new Date(
+                        System.currentTimeMillis()+1000*60*60
+                ))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser()
+                    .verifyWith( getSigningKey() )
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
 }
