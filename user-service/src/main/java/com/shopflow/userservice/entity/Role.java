d0a27fc1a82431dffe7aebc4d98187e9190a893d
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-09-19 23:06:21 +0530

    feat: add role to user entity

---
+++ user-service/src/main/java/com/shopflow/userservice/entity/Role.java
@@ -0,0 +1,6 @@
+package com.shopflow.userservice.entity;
+
+public enum Role{
+        USER,
+        ADMIN
+}
--- user-service/src/main/java/com/shopflow/userservice/entity/User.java
+++ user-service/src/main/java/com/shopflow/userservice/entity/User.java
@@ -23,4 +23,8 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
